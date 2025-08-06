e9c084344636a67922cb67d6a87e679107f4aad4
somnathd <somnathd@smartek21.com>
2025-08-07 00:54:22 +0530

    login and signup

--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,22 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { User } from '@app/users'; // <-- import from users lib
+import { ConfigService } from '@nestjs/config';
+import { createTypeOrmOptions } from './typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: 5432,
-      username: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASS || 'postgres',
-      database: process.env.DB_NAME || 'RAG_USER_DB',
-      entities: [User],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        await createTypeOrmOptions(configService),
     }),
   ],
 })
--- libs/database/src/typeorm.config.ts
+++ libs/database/src/typeorm.config.ts
@@ -1,16 +1,49 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
+import * as AWS from 'aws-sdk';
+import { User } from '@app/users'; // <-- import from users lib
+export async function createTypeOrmOptions(
+  configService: ConfigService,
+): Promise<TypeOrmModuleOptions> {
+  const isProd = configService.get('NODE_ENV') === 'production';
+  if (isProd) {
+    const secrets = await getAwsDbSecrets('pg-db-secret');
+    return {
+      type: 'postgres',
+      host: secrets.host,
+      port: Number(secrets.port),
+      username: secrets.username,
+      password: secrets.password,
+      database: secrets.dbname,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: false,
+      ssl: true,
+    };
+  }
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: '127.0.1.1',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'RAG_USER_DB',
-  logging: true,
-  entities: [
-    join(__dirname, '..', '..', '..', 'libs', '**', '*.entity.{js,ts}'),
-  ],
-  synchronize: true,
-};
+  // Local env config
+  return {
+    type: 'postgres',
+    host: configService.get('DB_HOST'),
+    port: Number(configService.get('DB_PORT')),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    entities: [User],
+    synchronize: true,
+  };
+}
+
+async function getAwsDbSecrets(secretName: string) {
+  const client = new AWS.SecretsManager({ region: 'your-region' });
+
+  const secretValue = await client
+    .getSecretValue({ SecretId: secretName })
+    .promise();
+
+  if (!secretValue || !secretValue.SecretString) {
+    throw new Error('Unable to retrieve DB secrets from AWS');
+  }
+
+  return JSON.parse(secretValue.SecretString);
+}
